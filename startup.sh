d5649c082c3bf0a715a5b297afe749a2117a7c39
Ilya Kulakov <ilyakulakov@MacBook-Air-Ilya.local>
2026-07-17 04:33:13 +0500

    Remove risky raw.githubusercontent re-download step, rely on git clone directly

--- startup.sh
+++ startup.sh
@@ -1,19 +1,2 @@
 #!/bin/bash
-cd /app/services
-python3 -c "
-import urllib.request
-files = ['signals.py', 'paper_trading.py', 'alerts.py']
-for f in files:
-    try:
-        urllib.request.urlretrieve(f'https://raw.githubusercontent.com/ilya4gabriel-star/fundamental-bot/main/services/{f}', f)
-    except: pass
-"
-cd /app
-python3 -c "
-import urllib.request
-for f in ['handlers/commands.py', 'main.py']:
-    try:
-        urllib.request.urlretrieve(f'https://raw.githubusercontent.com/ilya4gabriel-star/fundamental-bot/main/{f}', f)
-    except: pass
-"
 python3 main.py
